4a01fb6f6ce1aa1fa81845c1646180e49c3272a1
rtm516 <ryantmilner@hotmail.co.uk>
2025-11-21 16:06:56 +0000

    Add copy button for packet data

--- src/ui/src/components/DetailsPane.vue
+++ src/ui/src/components/DetailsPane.vue
@@ -7,12 +7,30 @@ import JsonViewer from './JsonViewer.vue';
 /** @type { { time: string, side: 'BEDROCK' | 'JAVA', direction: 'SERVERBOUND' | 'CLIENTBOUND', packetName: string, packetId: number, packetData: any } | null } */
 const selectedPacket = ref(null);
 
+const copied = ref(false);
+const copiedTimeout = ref(null);
+
 const scrollArea = useTemplateRef('scrollArea');
 
 EventBus.$on('select-packet', (packetInfo) => {
   selectedPacket.value = packetInfo;
   scrollArea.value.scrollTop = 0;
 });
+
+function copyToClip() {
+  if (selectedPacket.value === null) {
+    return;
+  }
+
+  const packetDataStr = JSON.stringify(selectedPacket.value.packetData, null, 2);
+  navigator.clipboard.writeText(packetDataStr);
+
+  copied.value = true;
+  copiedTimeout.value && clearTimeout(copiedTimeout.value);
+  copiedTimeout.value = setTimeout(() => {
+    copied.value = false;
+  }, 1000);
+}
 </script>
 
 <template>
@@ -28,6 +46,7 @@ EventBus.$on('select-packet', (packetInfo) => {
 
       <div class="card">
         <div class="card-body">
+          <button class="btn btn-sm btn-secondary float-end" :class="{'btn-success': copied}" @click="copyToClip"><i class="bi" :class="{'bi-clipboard': !copied, 'bi-clipboard-check': copied }"></i></button>
           <JsonViewer v-if="Object.keys(selectedPacket.packetData).length >= 1" :data="selectedPacket.packetData" />
           <div v-else>No packet data available.</div>
         </div>
